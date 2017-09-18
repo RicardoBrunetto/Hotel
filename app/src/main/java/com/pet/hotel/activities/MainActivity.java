eda291cbb30a09777da17204fbdbb7c10a83cd0b
Ricardo Brunetto <ra94182@uem.br>
2017-09-17 22:51:46 -0300

    Layouts e Componentes I

---
+++ app/src/main/java/com/pet/hotel/activities/MainActivity.java
@@ -0,0 +1,82 @@
+package com.pet.hotel.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.pet.hotel.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tab_host_main;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Adicionar Hotel", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show(); /*Implementação posterior*/
+            }
+        });
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+
+        tab_host_main = (TabLayout) findViewById(R.id.tabs);
+        tab_host_main.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        //adapter.addFragment(new OneFragment(), "ONE");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+}
---
+++ app/src/main/java/com/pet/hotel/controladoras/HotelController.java
@@ -0,0 +1,15 @@
+package com.pet.hotel.controladoras;
+
+import com.pet.hotel.dados.Hotel;
+
+import java.util.List;
+
+/**
+ * Created by rickh on 17/09/2017.
+ */
+
+public class HotelController {
+    public List<Hotel> geHoteisCadastrados(){
+        return null;
+    }
+}
